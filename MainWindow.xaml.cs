4382481ff998f0175e28d7c09e16f1d45eb31095
LingFeng-bbben <bengy@126.com>
2021-07-02 02:34:56 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Un4seen.Bass;
+
+namespace MajdataEdit
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        int bgmStream = -1024;
+        int clickStream = -8848;
+
+        string GetRawFumenText()
+        {
+            string text = "";
+            text = new TextRange(FumenContent.Document.ContentStart, FumenContent.Document.ContentEnd).Text;
+            text = text.Replace("\r","");
+            return text;
+        }
+
+        void SaveRawFumenText(bool writeToDisk = false)
+        {
+            if (selectedDifficulty == -1) return;
+            SimaiProcess.fumens[selectedDifficulty] = GetRawFumenText();
+            SimaiProcess.SaveData("maidata.txt");
+        }
+
+        void SetRawFumenText(string content)
+        {
+            FumenContent.Document.Blocks.Clear();
+            if (content == null) return;
+            string[] lines = content.Split('\n');
+            foreach (var line in lines)
+            {
+                Paragraph paragraph = new Paragraph();
+                paragraph.Inlines.Add(line);
+                FumenContent.Document.Blocks.Add(paragraph);
+            }
+        }
+
+        long GetRawFumenPosition()
+        {
+            long pos = new TextRange(FumenContent.Document.ContentStart, FumenContent.CaretPosition).Text.Replace("\r", "").Length;
+            return pos;
+        }
+
+        Timer currentTimeRefreshTimer = new Timer(5);
+        Timer clickSoundTimer = new Timer(10);
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var handle = (new WindowInteropHelper(this)).Handle;
+            Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_CPSPEAKERS,handle);
+            bgmStream = Bass.BASS_StreamCreateFile("track.mp3", 0L, 0L, BASSFlag.BASS_SAMPLE_FLOAT);
+            clickStream = Bass.BASS_StreamCreateFile("tap.mp3", 0L, 0L, BASSFlag.BASS_SAMPLE_FLOAT);
+
+            Bass.BASS_ChannelSetAttribute(bgmStream, BASSAttribute.BASS_ATTRIB_VOL, 0.7f);
+
+            currentTimeRefreshTimer.Elapsed += CurrentTimeRefreshTimer_Elapsed;
+            currentTimeRefreshTimer.Start();
+            clickSoundTimer.Elapsed += ClickSoundTimer_Elapsed;
+
+            SimaiProcess.ReadData("maidata.txt");
+        }
+
+
+        private void ClickSoundTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var currentTime = Bass.BASS_ChannelBytes2Seconds(bgmStream, Bass.BASS_ChannelGetPosition(bgmStream));
+            var waitToBePlayed = SimaiProcess.notelist.FindAll(o => o.havePlayed == false && o.time>currentTime);
+            //foreach(var a in waitToBePlayed) Console.WriteLine("Sort:"+a.time);
+            if (waitToBePlayed.Count < 1) return;
+            var nearestTime = waitToBePlayed[0].time;
+            //Console.WriteLine(nearestTime);
+            if (currentTime - nearestTime < 0.05 && currentTime - nearestTime > -0.05)
+            {
+                Bass.BASS_ChannelPlay(clickStream, true);
+                Console.WriteLine("Tick");
+                SimaiProcess.notelist.FindAll(o => o.havePlayed == false && o.time > currentTime)[0].havePlayed = true; //Since the data was added as time followed, we modify the first one
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            currentTimeRefreshTimer.Stop();
+        }
+
+        private void CurrentTimeRefreshTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var currentPlayTime = Bass.BASS_ChannelBytes2Seconds(bgmStream, Bass.BASS_ChannelGetPosition(bgmStream));
+            int minute = (int)currentPlayTime / 60;
+            double second = currentPlayTime % 60d;
+            Dispatcher.Invoke(new Action(()=>{ TimeLabel.Content = String.Format("{0}:{1:00}", minute, second); }));
+            
+        }
+
+        private void FumenContent_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void PlayAndPause_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Console.WriteLine("Executed");
+            var time = SimaiProcess.getSongTimeAndScan(GetRawFumenText(), GetRawFumenPosition());
+            Console.WriteLine(time);
+            Bass.BASS_ChannelSetPosition(bgmStream, time);
+            Bass.BASS_ChannelPlay(bgmStream, false);
+            SimaiProcess.ClearNoteListPlayedState();
+            clickSoundTimer.Start();
+        }
+
+        private void PlayAndPause_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            Console.WriteLine("CanExecute");
+            if (Bass.BASS_ChannelIsActive(bgmStream)==BASSActive.BASS_ACTIVE_PLAYING)
+            {
+                Bass.BASS_ChannelStop(bgmStream);
+                clickSoundTimer.Stop();
+                e.CanExecute = false;
+            }
+            else
+            {
+                e.CanExecute = true;
+            }
+        }
+
+        private void FumenContent_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            var time = SimaiProcess.getSongTimeAndScan(GetRawFumenText(), GetRawFumenPosition());
+            Console.WriteLine(time);
+            Bass.BASS_ChannelSetPosition(bgmStream, time);
+            SimaiProcess.ClearNoteListPlayedState();
+        }
+
+        int selectedDifficulty = -1;
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SaveRawFumenText();
+            ComboBoxItem selected = (ComboBoxItem)LevelSelector.SelectedItem;
+            for (int i = 0; i < 7; i++)
+            {
+                if (selected.Content.ToString() == ("&lv_" + (i + 1)))
+                {
+                    SetRawFumenText(SimaiProcess.fumens[i]);
+                    selectedDifficulty = i;
+                }
+            }
+        }
+
+        private void Menu_Open_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveRawFumenText(true);
+        }
+
+        private void Menu_SaveAs_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
---
+++ SimaiProcess.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MajdataEdit
+{
+    static class SimaiProcess
+    {
+        static public string title;
+        static public string artist;
+        static public string designer;
+        static public float first = 0;
+        static public string[] fumens = new string[7];
+        static public string[] levels = new string[7];
+        static public List<SimaiNote> notelist = new List<SimaiNote>();
+        static public void ReadData(string filename)
+        {
+            string[] maidataTxt = File.ReadAllLines(filename);
+            for (int i = 0; i < maidataTxt.Length; i++)
+            {
+                if (maidataTxt[i].StartsWith("&title="))
+                    title = GetValue(maidataTxt[i]);
+                if (maidataTxt[i].StartsWith("&artist="))
+                    artist = GetValue(maidataTxt[i]);
+                if (maidataTxt[i].StartsWith("&des="))
+                    designer = GetValue(maidataTxt[i]);
+                if (maidataTxt[i].StartsWith("&first="))
+                    first = float.Parse(GetValue(maidataTxt[i]));
+                for (int j = 1; j < 8 && i < maidataTxt.Length; j++)
+                {
+                    if (maidataTxt[i].StartsWith("&lv_" + j + "="))
+                        levels[j - 1] = GetValue(maidataTxt[i]);
+                    if (maidataTxt[i].StartsWith("&inote_" + j + "="))
+                    {
+                        string TheNote = "";
+                        TheNote += GetValue(maidataTxt[i]) + "\n";
+                        i++;
+                        for (; i < maidataTxt.Length; i++)
+                        {
+                            TheNote += maidataTxt[i] + "\n";
+                            if ((i + 1) < maidataTxt.Length)
+                            {
+                                if (maidataTxt[i + 1].StartsWith("&"))
+                                    break;
+                            }
+                        }
+                        fumens[j - 1] = TheNote;
+                    }
+                }
+
+            }
+            if (first != 0)
+            {
+                MessageBox.Show("本编辑器不想支持offset,请剪好了再来");
+            }
+            Console.WriteLine(fumens[5]);
+        }
+        static public void SaveData(string filename)
+        {
+            List<string> maidata = new List<string>();
+            maidata.Add("&title=" + title);
+            maidata.Add("&artist=" + artist);
+            maidata.Add("&first=0");
+            for (int i = 0; i < levels.Length; i++)
+            {
+                if (levels[i] != null && levels[i] != "")
+                {
+                    maidata.Add("&lv_" + i + "=" + levels[i]);
+                }
+            }
+            for (int i = 0; i < fumens.Length; i++)
+            {
+                if (fumens[i] != null && fumens[i] != "")
+                {
+                    maidata.Add("&inote_" + i + "=\n" + fumens[i]);
+                }
+            }
+            File.WriteAllLines(filename, maidata.ToArray());
+        }
+        static private string GetValue(string varline)
+        {
+            return varline.Split('=')[1];
+        }
+        static public double getSongTimeAndScan(string text, long position)
+        {
+            notelist.Clear();
+            try
+            {
+                int bpm = 0;
+                double time = 0; //in seconds
+                double requestedTime = 0;
+                int beats = 4;
+                bool haveNote = false;
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (i-1 < position)
+                    {
+                        requestedTime = time;
+                    }
+                    int dummy;
+                    if (int.TryParse(text[i].ToString(),out dummy))//if has number
+                    {
+                        haveNote = true;
+                    }
+                    if (text[i] == '(')
+                    //Get bpm
+                    {
+                        string bpm_s = "";
+                        i++;
+                        while (text[i] != ')')
+                        {
+                            bpm_s += text[i];
+                            i++;
+                        }
+                        bpm = int.Parse(bpm_s);
+                        Console.WriteLine("BPM" + bpm);
+                        continue;
+                    }
+                    if (text[i] == '{')
+                    //Get beats
+                    {
+                        string beats_s = "";
+                        i++;
+                        while (text[i] != '}')
+                        {
+                            beats_s += text[i];
+                            i++;
+                        }
+                        beats = int.Parse(beats_s);
+                        Console.WriteLine("BEAT" + beats);
+                        continue;
+                    }
+                    if (text[i] == ',')
+                    {
+                        if (haveNote)
+                        {
+                            notelist.Add(new SimaiNote(time));
+                        }
+                        time += (1d / (bpm / 60d)) * 4d / (double)beats;
+                        Console.WriteLine(time);
+                        haveNote = false;
+                        continue;
+                    }
+                }
+                Console.WriteLine(notelist.ToArray());
+                return requestedTime;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+        static public void ClearNoteListPlayedState()
+        {
+            notelist.Sort((x, y) => x.time.CompareTo(y.time));
+            for (int i = 0; i < notelist.Count; i++)
+            {
+                notelist[i].havePlayed = false;
+            }
+        }
+    }
+
+    class SimaiNote
+    {
+        public double time;
+        public bool havePlayed;
+        public SimaiNote(double _time)
+        {
+            time = _time;
+        }
+        //TODO: add some type here
+    }
+}
