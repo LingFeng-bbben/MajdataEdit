d4256608fd2b1036d38e41325e16aa1ef8499944
Moying-moe <741744469@qq.com>
2021-08-30 04:07:56 +0800

    当报错 需要强制软件渲染时 弹出更用户友好的弹窗提示 但是我的电脑并不能复现这个问题 所以需要一名小白鼠来进行测试

--- App.xaml.cs
+++ App.xaml.cs
@@ -21,6 +21,15 @@ namespace MajdataEdit
         }
         private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
+            if(e.Exception.GetType() == typeof(System.Runtime.InteropServices.COMException) && 
+                e.Exception.Message.IndexOf("UCEERR_RENDERTHREADFAILURE") != -1)
+            {
+                // 需要开启软件渲染
+                MessageBox.Show(MajdataEdit.MainWindow.GetLocalizedString("SoftRenderError"), MajdataEdit.MainWindow.GetLocalizedString("Error"));
+                this.Shutdown(114);
+                return;
+            }
+
             MessageBox.Show(e.Exception.Source+" At:\n"+e.Exception.Message+"\n"+e.Exception.StackTrace, "发生错误", MessageBoxButton.OK, MessageBoxImage.Error);
             e.Handled = true;
         }
