05eeb5bc9c823de85a98377e288aa64dcaae70df
Moying-moe <741744469@qq.com>
2022-11-01 15:32:03 +0800

    生成设置

--- MainWindowCore.cs
+++ MainWindowCore.cs
@@ -34,7 +34,7 @@ namespace MajdataEdit
 {
     public partial class MainWindow : Window
     {
-        public static readonly string MAJDATA_VERSION_STRING = "v4.0-rc";
+        public static readonly string MAJDATA_VERSION_STRING = "v4.0.0-rc";
         public static readonly SemVersion MAJDATA_VERSION = SemVersion.Parse(MAJDATA_VERSION_STRING, SemVersionStyles.Any);
         bool UpdateCheckLock = false;
 
