a15d97332fca3053441418952e5cc64133942ab4
LingFeng-bbben <bengy@126.com>
2023-03-22 04:46:07 +0800

    update version

--- MainWindowCore.cs
+++ MainWindowCore.cs
@@ -35,7 +35,7 @@ namespace MajdataEdit
 {
     public partial class MainWindow : Window
     {
-        public static readonly string MAJDATA_VERSION_STRING = "v4.1.0";
+        public static readonly string MAJDATA_VERSION_STRING = "v4.1.1";
         public static readonly SemVersion MAJDATA_VERSION = SemVersion.Parse(MAJDATA_VERSION_STRING, SemVersionStyles.Any);
         bool UpdateCheckLock = false;
 
